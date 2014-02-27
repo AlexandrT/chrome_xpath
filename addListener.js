703d43db40f1fee38382c299414fb61c3e6fbbb9
Alexandr T <leshrac88@gmail.com>
2014-02-27 09:07:03 -0500

    Try to use localStorage for communication tab with extension

--- addListener.js
+++ addListener.js
@@ -1,33 +1,48 @@
-var collection = document.getElementsByTagName("*");
-for (var i = 0; collection && collection.length && i < collection.length; i++) {
-  collection[i].addEventListener('click', getXpath);
-}
-
-function getXpath(event) {
-  var arr = [];
-  var DOMelem = this;
-  var partXpath = DOMelem.getAttribute("id");
-  if (partXpath != null) {
-      arr.push(DOMelem.tagName + "//id = " + partXpath);
-  } else {
-    partXpath = DOMelem.getAttribute("class");
-    if (partXpath != null) {
-        arr.push(DOMelem.tagName + "//class = " + partXpath);
-    // } else if (typeof DOMelem.parent != "undefined") {
-        // getXpath(DOMelem.parent);
-    } else {
-      console.log("impossible parse");
-    }
-  }
-  for (var i = 0; collection && collection.length && i < collection.length; i++) {
-    collection[i].removeEventListener('click', getXpath, false);
-  }
-  // console.log(arr);
-  window.fullXpath = arr.join();
-  console.log(window.fullXpath);
-}
-
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  // chrome.tabs.sendMessage(sender.id, "response");
-  chrome.runtime.sendMessage(sender.id, window.fullXpath);
+  console.log(message);
+  if (message.method == "getLocalStorage")
+    sendResponse({data: localStorage[message.key]});
+  else
+    sendResponse({data: 'before'});
+    var collection = document.getElementsByTagName("*");
+    for (var i = 0; collection && collection.length && i < collection.length; i++) {
+      collection[i].addEventListener('click', getXpath);
+    }
+
+    function getXpath(event) {
+      var arr = [];
+      var DOMelem = this;
+      var partXpath = DOMelem.getAttribute("id");
+      if (partXpath != null) {
+          arr.push(DOMelem.tagName + "//id = " + partXpath);
+      } else {
+        partXpath = DOMelem.getAttribute("class");
+        if (partXpath != null) {
+            arr.push(DOMelem.tagName + "//class = " + partXpath);
+        // } else if (typeof DOMelem.parent != "undefined") {
+            // getXpath(DOMelem.parent);
+        } else {
+          console.log("impossible parse");
+        }
+      }
+      for (var i = 0; collection && collection.length && i < collection.length; i++) {
+        collection[i].removeEventListener('click', getXpath, false);
+      }
+      // console.log(arr);
+      fullXpath = arr.join();
+      // console.log(fullXpath);
+
+      saveToStorage(message, fullXpath);
+    }
+
+    function saveToStorage(key, value){
+      // chrome.storage.local.set({key: value}, function(){
+        localStorage[key] = value;
+        // console.log(key + ": " + value);
+    }
+
+    // chrome.tabs.sendMessage(sender.id, "response");
+    //chrome.runtime.sendMessage(sender.id, window.fullXpath);
+
+    sendResponse({data: 'lol'});
 });
--- popup.js
+++ popup.js
@@ -1,22 +1,47 @@
+// $(document).ready(function(){
+// 	var tempArr = [];
+// 	var resultArr = [];
+// 	tempArr = document.getElementById('fields').getElementsByTagName('button');
+// 	for (var i = 0; i < tempArr.length; i++){
+// 		resultArr.push(tempArr[i].getAttribute('class'));
+// 	}
+// 	loadFromStorage(resultArr);
+// })
+
 $(function() {
   $('div > button').click(function() { //change selector, something... div#fields > button
-  	console.log(this);
-  	// var window.xPathButtonClass = this.getAttribute("class");
+  	//console.log(this);
+  	var xPathType = this.getAttribute("class");
   	// console.log(buttonId);
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       var tabId = tabs[0].id;
-      //sendMessage(tabId);
+      sendMessage(tabId, xPathType);
       chrome.tabs.executeScript(null, {file: "addListener.js" });
+
+      chrome.runtime.sendMessage({method: "getLocalStorage", key: xPathType}, function(response) {
+  			console.log(response);
+			});
     });
   });
 });
 
-function sendMessage(lastTabId){
-	chrome.tabs.sendMessage(lastTabId, "Background page started.");
+function sendMessage(lastTabId, type){
+	chrome.tabs.sendMessage(lastTabId, type);
 }
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  console.log("received message: " + message);
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+  // console.log("received message: " + message);
   // var elem = 'input.' + window.xPathButtonClass
   // $(elem).attr("value", message);
-});
+// });
+
+
+// function loadFromStorage(arrayOfKeys){
+// 	arrayOfKeys.forEach(function(key){
+// 		// chrome.tabs.executeScript(null, {code: 'var ' + key + ' = "";' });
+// 		// chrome.storage.local.get(key, function(result){
+// 			// chrome.tabs.executeScript(null, {code: key + ' = result.' + key + ';' });
+// 			// $('input.' + key).val(key);
+// 			console.log(localStorage[key]);
+// 	});
+// }
