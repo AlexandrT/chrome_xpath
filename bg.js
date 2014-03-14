d3aa7a609fa36a3841b631dac8ad68d1865d7095
Alexandr T <leshrac88@gmail.com>
2014-03-14 10:37:18 -0400

    send xpath from content to background

--- bg.js
+++ bg.js
@@ -1,71 +1,23 @@
-// $(document).ready(function(){
-// 	var tempArr = [];
-// 	var resultArr = [];
-// 	tempArr = document.getElementById('fields').getElementsByTagName('button');
-// 	for (var i = 0; i < tempArr.length; i++){
-// 		resultArr.push(tempArr[i].getAttribute('class'));
-// 	}
-// 	loadFromStorage(resultArr);
-// })
-
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
-});
-
 $(window).load(function() {
   console.log("window load");
-
-  chrome.extension.onConnect.addListener(function(port){
-    port.onMessage.addListener(factory);
-  });
-
-
+  
   chrome.tabs.onUpdated.addListener(function(tabId, tabInfo, tab){
+
+    chrome.tabs.connect(tabId);
+
+    chrome.extension.onConnect.addListener(function(port){
+      port.onMessage.addListener(function(msg){
+        console.log(msg);
+      });
+    });
+
+
     console.log("change active tab");
     console.log(tabId);
 
-    chrome.tabs.executeScript(tabId, { code: "addListener()" })
-
+    chrome.commands.onCommand.addListener(function(command) {
+      console.log('Command:', command);
+      chrome.tabs.executeScript(tabId, { code: "addListener()" })
+    });
   })
-
-  /*chrome.tabs.onUpdated.addListener(function(tabId, tabInfo, tab){
-    console.log("tab updated");
-    console.log(tabId);
-    console.log(tabInfo);
-    console.log(tab);
-  })*/
-  // $(function() {
-  //   $('div > button').click(function() {
-  //     var xPathType = this.getAttribute("class");
-  //     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  //       var tabId = tabs[0].id;
-  //       sendMessage(tabId, xPathType);
-  //       chrome.tabs.executeScript(null, {file: "addListener.js" });
-
-  //       chrome.runtime.sendMessage({method: "getLocalStorage", key: xPathType}, function(response) {
-  //         console.log(response);
-  //       });
-  //     });
-  //   });
-
-  // function sendMessage(lastTabId, type){
-  //   chrome.tabs.sendMessage(lastTabId, type);
-  // }
-
-  // chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-    // console.log("received message: " + message);
-    // var elem = 'input.' + window.xPathButtonClass
-    // $(elem).attr("value", message);
-  // });
-
-
-  // function loadFromStorage(arrayOfKeys){
-  //  arrayOfKeys.forEach(function(key){
-  //    // chrome.tabs.executeScript(null, {code: 'var ' + key + ' = "";' });
-  //    // chrome.storage.local.get(key, function(result){
-  //      // chrome.tabs.executeScript(null, {code: key + ' = result.' + key + ';' });
-  //      // $('input.' + key).val(key);
-  //      console.log(localStorage[key]);
-  //  });
-  // }
 })
--- content.js
+++ content.js
@@ -1,6 +1,8 @@
-chrome.extension.onConnect.addListener(function(port){
-  port.onMessage.addListener(factory);
-});
+var port = chrome.runtime.connect({name: "sendXpath"});
+port.onMessage.addListener(function(msg){
+  console.log("new message!");
+  console.log(msg);
+})
 
 function addListener(){
   var collection = document.getElementsByTagName("*");
@@ -27,59 +29,9 @@ function addListener(){
     for (var i = 0; collection && collection.length && i < collection.length; i++) {
       collection[i].removeEventListener('click', getXpath, false);
     }
-    // console.log(attrForXpath);
+
     fullXpath = attrForXpath.join();
     console.log(fullXpath);
-
-    //saveToStorage(message, fullXpath);
+    port.postMessage(fullXpath);
   }
 }
-
-/*chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-  console.log(message);
-  if (message.method == "getLocalStorage")
-    sendResponse({data: localStorage[message.key]});
-  else
-    sendResponse({data: 'before'});
-    var collection = document.getElementsByTagName("*");
-    for (var i = 0; collection && collection.length && i < collection.length; i++) {
-      collection[i].addEventListener('click', getXpath);
-    }
-
-    function getXpath(event) {
-      var arr = [];
-      var DOMelem = this;
-      var partXpath = DOMelem.getAttribute("id");
-      if (partXpath != null) {
-          arr.push(DOMelem.tagName + "//id = " + partXpath);
-      } else {
-        partXpath = DOMelem.getAttribute("class");
-        if (partXpath != null) {
-            arr.push(DOMelem.tagName + "//class = " + partXpath);
-        // } else if (typeof DOMelem.parent != "undefined") {
-            // getXpath(DOMelem.parent);
-        } else {
-          console.log("impossible parse");
-        }
-      }
-      for (var i = 0; collection && collection.length && i < collection.length; i++) {
-        collection[i].removeEventListener('click', getXpath, false);
-      }
-      // console.log(arr);
-      fullXpath = arr.join();
-      // console.log(fullXpath);
-
-      saveToStorage(message, fullXpath);
-    }
-
-    function saveToStorage(key, value){
-      // chrome.storage.local.set({key: value}, function(){
-        localStorage[key] = value;
-        // console.log(key + ": " + value);
-    }
-
-    // chrome.tabs.sendMessage(sender.id, "response");
-    //chrome.runtime.sendMessage(sender.id, window.fullXpath);
-
-    sendResponse({data: 'lol'});
-});*/
