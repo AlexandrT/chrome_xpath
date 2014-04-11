c8e7ac9a9e09344d48f6b95dc27257c974f4426b
Alexandr T <leshrac88@gmail.com>
2014-04-11 11:57:36 +0400

    first test

--- prepare_tests.sh
+++ prepare_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mv -f ./test/SpecRunner.html ./vendor/jasmine/
+mv -f ./test/SpecRunner.html ./vendor/jasmine/2.0.0/
---
+++ test/spec/contentSpec.js
@@ -0,0 +1,8 @@
+describe("build xpath", function(){
+	var arr = [];
+
+	it("revert and join array", function(){
+		arr = ["1", "2", "3"];
+		expect(buildXpath(arr)).toEqual("321");
+	});
+});
