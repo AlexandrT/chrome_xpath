815c1bbbf982948f8fe9d5299cd440a75cfc3cee
AlexandrT <klipmash@gmail.com>
2014-04-16 09:00:23 +0400

    tests started

--- test/runTests.js
+++ test/runTests.js
@@ -1,9 +1,19 @@
 var page = require('webpage').create();
-page.open('../vendor/components-jasmine/SpecRunner.html', function(){
-	setTimeout(function(){
-		var content = page.evaluate(function() {
-			return window.body;
-		});
-		phantom.exit();
-	}, 20000);
+	system = require('system');
+
+if (system.args.length === 1) {
+	console.log('url not specified');
+	phantom.exit();
+}
+
+var address = system.args[1];
+// page.open('../vendor/components-jasmine/SpecRunner.html', function(){
+page.open(address, function(){
+	page.injectJs('../lib/jquery-2.1.0.min.js');
+		
+	var content = page.evaluate(function() {
+		return $('html').html();
+	});
+	console.log(content);
+	phantom.exit();
 });
